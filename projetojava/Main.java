5a62bf10d4330aa6d526fd1e3bcd6ae6da456225
Lucas <camargolimalucas@gmail.com>
2023-12-22 11:05:48 -0300

    envio variaveis-lacos-main

---
+++ projetojava/Main.java
@@ -0,0 +1,72 @@
+package projetojava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Camiseta camiseta1 = new Camiseta("Manga Curta", 10.00);
+		Camiseta camiseta2 = new Camiseta("Manga longa", 20.00);
+		Camiseta camiseta3 = new Camiseta("Xadrez", 30.00);
+
+		List<Carrinho> carrinhoDeCompras = new ArrayList<Carrinho>();
+
+		Scanner leia = new Scanner(System.in);
+		int opcao, qtde;
+
+		do {
+			System.out.println("		BRECHÓ	GEN\r\n" + "\r\n" + "Item		Camiseta		Valor\r\n"
+					+ "1.		Manga curta		R$ 10,00\r\n" + "2.		Manga longa		R$ 20,00\r\n"
+					+ "3.		Xadrez			R$ 30,00\r\n" + "0. 		Sair");
+			System.out.println("Escolha uma opção:");
+			opcao = leia.nextInt();
+
+			if (opcao >= 1 && opcao <= 3) {
+				System.out.println("Digite a quantidade desejada:");
+				qtde = leia.nextInt();
+				adicionarAoCarrinho(carrinhoDeCompras, opcao, qtde);
+			} else if (opcao != 0) {
+				System.out.println("Opção inválida. Tente novamente.");
+			}
+
+		} while (opcao != 0);
+
+		imprimirResumoCarrinho(carrinhoDeCompras);
+
+		System.out.println("Programa encerrado.");
+	}
+
+	private static void adicionarAoCarrinho(List<Carrinho> carrinho, int escolha, int quantidade) {
+		Camiseta opcaoEscolhida = null;
+
+		switch (escolha) {
+		case 1:
+			opcaoEscolhida = new Camiseta("Manga curta", 10.00);
+			break;
+		case 2:
+			opcaoEscolhida = new Camiseta("Manga longa", 20.00);
+			break;
+		case 3:
+			opcaoEscolhida = new Camiseta("Xadrez", 30.00);
+			break;
+		}
+
+		double total = opcaoEscolhida.getPreco() * quantidade;
+		Carrinho item = new Carrinho(opcaoEscolhida, quantidade, total);
+		carrinho.add(item);
+
+		System.out.println("Produto adicionado ao carrinho!");
+	}
+
+	private static void imprimirResumoCarrinho(List<Carrinho> carrinho) {
+		System.out.println("------- Resumo do Carrinho -------");
+		for (Carrinho item : carrinho) {
+			System.out.println("Tipo: " + item.getCamiseta().getTipo());
+			System.out.println("Quantidade: " + item.getQuantidade());
+			System.out.println("Valor Total: R$ " + item.getTotal());
+			System.out.println("----------------------------------");
+		}
+
+	}
+}
